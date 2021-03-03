48f779329483351b7c25bcb1055bf4bca02fcd2a
fracogno <francesco.cognolato@gmail.com>
2021-03-03 13:09:24 +0000

    App prediction

--- ml/app.py
+++ ml/app.py
@@ -1,28 +1,27 @@
 from flask import Flask, jsonify, make_response, request
+import numpy as np
 
 from src.models import task_1 as models_t1
 from src.utils import misc
 app = Flask(__name__)
 
 base_path = misc.get_base_path()
+normalization_by_feature = misc.load_pickle(base_path + "pickles/task_1_max.py")
 
 
-@app.route("/typeform/task_1", methods=['GET'])
+@app.route("/typeform/task_1", methods=['POST'])
 def task_1():
-    if "data" not in request.args:
+    if "data" not in request.json:
         return make_response(jsonify({"error": "Missing data field."}), 400)
 
-    data = request.args["data"]
-    return jsonify({"prediction": data})
-    ##
-    #
-    #model = models_t1.get_MLP()
+    data = np.array(request.json["data"])
+    if len(data.shape) != 2 or data.shape[1] != 47:
+        return make_response(jsonify({"error": "Wrong data shape."}), 400)
 
-    normalization_by_feature = misc.load_pickle(base_path + "pickles/task_1_max.py")
-    #data /= normalization_by_feature
-
-    #model.load_weights(base_path + "checkpoints/task_1/model")
-    #return jsonify({"prediction": model.predict(data)})
+    model = models_t1.get_MLP()
+    model.load_weights(base_path + "checkpoints/task_1/model")
+    data /= normalization_by_feature
+    return jsonify({"prediction": str(model.predict(data))})
 
 
 if __name__ == "__main__":
